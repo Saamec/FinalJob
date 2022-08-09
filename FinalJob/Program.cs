2e7b00ccaa5241a74728747b344e9ddc45af5472
«Ilya <333zero777@mail.ru>
2022-08-09 19:46:23 +0300

    Инициализация массива

--- FinalJob/Program.cs
+++ FinalJob/Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("Задайте массив строк");
+Console.WriteLine("Укажите количество элементов массива");
+int countMass = 0;
+try
+{
+    countMass = Convert.ToInt32(Console.ReadLine());
+}catch(Exception ex) { Console.WriteLine("Введено не число"); }
+int count = 0;
+String [] array = new String [countMass];
+while (count != countMass)
+{
+    array [count] = Console.ReadLine();
+    count++;
+}
